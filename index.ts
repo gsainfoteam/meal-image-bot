41195dcd7e808a34024756b3cd7731668496b1d5
2paperstar <lee@paperst.ar>
2026-03-04 22:58:40 +0900

    feat: default get

--- index.ts
+++ index.ts
@@ -1,27 +1,25 @@
 import { App } from "@slack/bolt";
 
-// Initialize your app with your bot token and app token (or use environment variables)
 const app = new App({
   token: Bun.env.SLACK_BOT_TOKEN,
   appToken: Bun.env.SLACK_APP_TOKEN,
   signingSecret: Bun.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // Use socketMode for development without a public URL
+  socketMode: Bun.env.NODE_ENV === "development",
 });
 
-// Listen for the slash command
 app.command("/meal", async ({ command, ack, respond }) => {
-  // Acknowledge the command request immediately
   await ack();
 
+  const response = await fetch(
+    "https://www.gist.ac.kr/kr/html/sub05/050602.html",
+  );
+  const html = await response.text();
   const user = command.user_name;
-  const text = command.text; // The text entered after the command
 
-  // Respond to the user
-  await respond(`Hello, ${user}! You typed: "${text}".`);
+  await respond(`Hello, ${user}! You typed: "${html.slice(0, 1000)}".`);
 });
 
 (async () => {
-  // Start the app
   await app.start();
   console.log("⚡️ Bolt app is running!");
 })();
