a04bef93190bb7efeeff4703139278aeff72dba6
2paperstar <lee@paperst.ar>
2026-03-15 03:17:15 +0900

    feat: response with file

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
 import { App } from "@slack/bolt";
+import { load } from "cheerio";
 
 const app = new App({
   token: Bun.env.SLACK_BOT_TOKEN,
   appToken: Bun.env.SLACK_APP_TOKEN,
   signingSecret: Bun.env.SLACK_SIGNING_SECRET,
-  socketMode: Bun.env.NODE_ENV === "development",
+  socketMode: true,
 });
 
-app.command("/meal", async ({ command, ack, respond }) => {
-  await ack();
+const getFile = async (type: "1" | "2") => {
+  const link = `https://www.gist.ac.kr/kr/html/sub05/05060${type}.html`;
+  const response = await fetch(link);
+  const $ = load(await response.text());
+  const image = $(".bd_boxtype:first-child img").attr("src");
+  const imageUrl = `${link}${image}`;
+  const imageResponse = await fetch(imageUrl);
+  const buffer = Buffer.from(await imageResponse.arrayBuffer());
+  return buffer;
+};
 
-  const response = await fetch(
-    "https://www.gist.ac.kr/kr/html/sub05/050602.html",
-  );
-  const html = await response.text();
-  const user = command.user_name;
-
-  await respond(`Hello, ${user}! You typed: "${html.slice(0, 1000)}".`);
+app.event("app_mention", async ({ event, client, say }) => {
+  await client.filesUploadV2({
+    file_uploads: [
+      {
+        file: await getFile("1"),
+        filename: "meal1.png",
+      },
+      {
+        file: await getFile("2"),
+        filename: "meal2.png",
+      },
+    ],
+    channel_id: event.channel,
+  });
 });
 
 (async () => {
